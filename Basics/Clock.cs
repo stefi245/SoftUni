221f5fdbd3a15ee3e46317533fbb150ac7045e86
stefi245 <145605678+stefi245@users.noreply.github.com>
2023-09-21 20:39:55 +0200

    Create Clock.cs

---
+++ Basics/Clock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            for (int h = 0; h <= 23; h++)
+            {
+                for (int m = 0; m <= 59; m++)
+                {
+                    Console.WriteLine($"{h}:{m}");
+                }
+            }
+        }
+    }
+}
