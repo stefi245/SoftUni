eb05c5c5b196b27857d040118dc30848d020ff25
stefi245 <145605678+stefi245@users.noreply.github.com>
2023-09-23 10:42:26 +0200

    Task StudentInformation and Passed added

---
+++ Fundamentals/Lab_Intro_and_Basic_Syntax/01.StudentInformation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabIntroAndBasicSyntax
+{
+    //"Name: {student name}, Age: {student age}, Grade: {student grade}".
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double averageGrade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {averageGrade:f2}");
+
+        }
+    }
+}
---
+++ Fundamentals/Lab_Intro_and_Basic_Syntax/02.Passed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Passed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
