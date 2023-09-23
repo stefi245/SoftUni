b0901885db2ad62c5bd4140b742844c19179e0dd
stefi245 <145605678+stefi245@users.noreply.github.com>
2023-09-23 14:52:39 +0200

    New projects added to Fundamentals

---
+++ Fundamentals/Lab_Intro_and_Basic_Syntax/10.MultipilicationTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0; 
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sum = number * i; 
+                Console.WriteLine($"{number} X {i} = {sum}");
+            }
+        }
+    }
+}
