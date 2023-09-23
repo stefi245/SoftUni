18f731ca6cbaf34d1e9372c867c4088296e4d888
stefi245 <145605678+stefi245@users.noreply.github.com>
2023-09-23 13:43:46 +0200

    Create 08.DivisibeBy3.cs

---
+++ Fundamentals/Lab_Intro_and_Basic_Syntax/08.DivisibeBy3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Divisible_by_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=100; i+=3) 
+            {
+                if (i%3 == 0)
+                {
+                    Console.WriteLine(i); 
+                }
+            }
+        }
+    }
+}
